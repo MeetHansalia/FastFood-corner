69e18c19ed647e7ee447ff0a713c9dcc63f4a5f7
meet_hansalia <meethansalia99@gmail.com>
2023-06-19 16:38:41 +0530

    added useeffect timerSetout

--- fastfood/src/store/CartProvider.js
+++ fastfood/src/store/CartProvider.js
@@ -10,8 +10,28 @@ const defaultCartState = {
 
 const cartReducer=(state, action) => {
     if(action.type === "ADD"){
-        const updatedItems = state.items.concat(action.item);
         const updatedTotalAmount = state.totalAmount + action.totalAmount * action.item.amount;
+
+        const existingcartItemIndex = state.items.findIndex(
+            (item)=> item.id === action.item.id
+        );
+        
+        const existingCartItem = state.items[existingcartItemIndex];
+        let updatedItems;
+        
+        if(existingCartItem){
+            const updatedItem={
+            
+                ...existingCartItem,
+                amount: existingCartItem.amount + action.item.amount
+            };
+            updatedItems=[...state.items];
+            updatedItems[existingcartItemIndex]= updatedItem;
+        }else {            
+            updatedItems = state.items.concat(action.item);
+        }
+        
+        
 
         return{
             items : updatedItems,
